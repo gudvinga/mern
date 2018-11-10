67e688f3005e789c81b8bf9016ba9ed154c56928
gudvinga <gudvinga@gmail.com>
2018-11-10 17:50:44 +0300

    updated port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello express!'));
 
-app.listen(3000, () => console.log('Server is running on port :3000'));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
