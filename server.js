772e13a3de8923438c18d3e1537029906f297a4b
Aliaksei Hudzimchyk <31861355+gudvinga@users.noreply.github.com>
2019-04-11 23:34:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello express!'));
+app.get('/ip-checker', (req, res) => {
+ const ip =
+  req.headers['x-forwarded-for'] ||
+  req.connection.remoteAddress ||
+  req.socket.remoteAddress ||
+  (req.connection.socket
+    ? req.connection.socket.remoteAddress
+    : null);
+  res.send('Your ip is ' + ip);
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
